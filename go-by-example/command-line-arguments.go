2ede4f9fc0b02780805332d2b17f2a6705b1cd87
wuzhihao-jk <wuzhihao-jk@360shuke.com>
2022-04-20 16:35:26 +0800

    let's go

--- go-by-example/command-line-arguments.go
+++ go-by-example/command-line-arguments.go
@@ -7,10 +7,12 @@ import (
 
 func main() {
 
+	// os.Args 提供原始命令行参数访问功能。 注意，切片中的第一个参数是该程序的路径， 而 os.Args[1:]保存了程序全部的参数。
 	argsWithProg := os.Args
 	argsWithoutProg := os.Args[1:]
 
-	arg := os.Args[3]
+	// 你可以使用标准的下标方式取得单个参数的值。
+	arg := os.Args[0]
 
 	fmt.Println(argsWithProg)
 	fmt.Println(argsWithoutProg)
--- go-by-example/command-line-subcommands.go
+++ go-by-example/command-line-subcommands.go
@@ -8,6 +8,11 @@ import (
 
 func main() {
 
+	// go 和 git 这种命令行工具，都有很多的 子命令 。
+	//并且每个工具都有一套自己的 flag，比如： go build 和 go get 是 go 里面的两个不同的子命令。
+	//flag 包让我们可以轻松的为工具定义简单的子命令。
+
+	// 我们使用 NewFlagSet 函数声明一个子命令， 然后为这个子命令定义一个专用的 flag。
 	fooCmd := flag.NewFlagSet("foo", flag.ExitOnError)
 	fooEnable := fooCmd.Bool("enable", false, "enable")
 	fooName := fooCmd.String("name", "", "name")
@@ -15,13 +20,15 @@ func main() {
 	barCmd := flag.NewFlagSet("bar", flag.ExitOnError)
 	barLevel := barCmd.Int("level", 0, "level")
 
+	// 子命令应作为程序的第一个参数传入
 	if len(os.Args) < 2 {
 		fmt.Println("expected 'foo' or 'bar' subcommands")
 		os.Exit(1)
 	}
 
+	// 检查哪一个子命令被调用了
+	// 每个子命令，都会解析自己的 flag 并允许它访问后续的位置参数。
 	switch os.Args[1] {
-
 	case "foo":
 		fooCmd.Parse(os.Args[2:])
 		fmt.Println("subcommand 'foo'")
