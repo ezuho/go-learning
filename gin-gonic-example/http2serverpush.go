7398304312ddb32d05efce7a033d87cddf1f63ae
wuzhihao-jk <wuzhihao-jk@360shuke.com>
2022-05-10 14:17:49 +0800

    http2 server push

---
+++ gin-gonic-example/http2serverpush.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var html = template.Must(template.New("https").Parse(`
+<html>
+<head>
+  <title>Https Test</title>
+  <script src="/assets/app.js"></script>
+</head>
+<body>
+  <h1 style="color:red;">Welcome, Ginner!</h1>
+</body>
+</html>
+`))
+
+// see https://go.dev/blog/h2push
+func main() {
+	r := gin.Default()
+	r.Static("/assets", "./assets")
+	r.SetHTMLTemplate(html)
+
+	r.GET("/", func(c *gin.Context) {
+		if pusher := c.Writer.Pusher(); pusher != nil {
+			// use pusher.Push() to do server push
+			if err := pusher.Push("/assets/app.js", nil); err != nil {
+				log.Printf("Failed to push: %v", err)
+			}
+		}
+		c.HTML(http.StatusOK, "https", gin.H{
+			"status": "success",
+		})
+	})
+
+	// Listen and Server in https://127.0.0.1:8080
+	r.RunTLS(":8080", "./testdata/server.pem", "./testdata/server.key")
+}
