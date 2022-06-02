735732a3db08f69a6f263e369e81e39dc23adbce
wuzhihao-jk <wuzhihao-jk@360shuke.com>
2022-06-02 15:59:27 +0800

    servingdatafromreader.go servingstaticfiles.go

---
+++ gin-gonic-example/servingstaticfiles.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.Static("/assets", "./assets")
+	r.StaticFS("/more_static", http.Dir("my_file_system"))
+	r.StaticFile("/favicon.ico", "./resources/favicon.ico")
+
+	r.Run(":8080")
+}
